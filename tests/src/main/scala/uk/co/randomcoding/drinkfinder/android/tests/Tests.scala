c6e10f7645a5de5ba1f1cc4f6f221e395302d76d
randomcoder <randomcoder@randomcoding.co.uk>
2012-07-11 08:30:42 +0100

    Initial commit of integration tests project.

---
+++ tests/src/main/scala/uk/co/randomcoding/drinkfinder/android/tests/Tests.scala
@@ -0,0 +1,20 @@
+package uk.co.randomcoding.drinkfinder.android.tests
+
+import android.test.AndroidTestCase
+import junit.framework.Assert.assertEquals
+import uk.co.randomcoding.drinkfinder.android.MainActivity
+import uk.co.randomcoding.drinkfinder.android.TR
+
+class AndroidTests extends AndroidTestCase {
+  def testPackageIsCorrect() {
+    assertEquals("uk.co.randomcoding.drinkfinder.android", getContext.getPackageName)
+  }
+}
+
+class ActivityTests extends ActivityInstrumentationTestCase2(classOf[MainActivity]) {
+   def testHelloWorldIsShown() {
+      val activity = getActivity
+      val textview = activity.findView(TR.textview)
+      assertEquals(textview.getText, "hello, world!")
+    }
+}
