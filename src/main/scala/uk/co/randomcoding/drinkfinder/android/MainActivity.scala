05a907bdd7e51a477e68f76eb3caa6de9b9d6be6
randomcoder <randomcoder@randomcoding.co.uk>
2012-07-11 22:37:00 +0100

    Added buttons to layout

--- src/main/scala/uk/co/randomcoding/drinkfinder/android/MainActivity.scala
+++ src/main/scala/uk/co/randomcoding/drinkfinder/android/MainActivity.scala
@@ -2,12 +2,25 @@ package uk.co.randomcoding.drinkfinder.android
 
 import android.app.Activity
 import android.os.Bundle
+import uk.co.randomcoding.drinkfinder.android._
+import android.view.View
 
 class MainActivity extends Activity with TypedActivity {
   override def onCreate(bundle: Bundle) {
     super.onCreate(bundle)
     setContentView(R.layout.main)
-
-    findView(TR.textview).setText("hello, world!")
+  
+  }
+  
+  def showSearchDrinks(view: View) {
+	  // TODO: create intent to switch to the search view
+  }
+  
+  def updateData(view: View) {
+	  // TODO: Update the data from the online store and save in zipped file
+  }
+  
+  def showWishList(view: View) {
+	  // TODO: Create intent to switch to the wishlist view
   }
 }
---
+++ src/main/scala/uk/co/randomcoding/drinkfinder/android/SearchDrinkActivity.scala
@@ -0,0 +1,22 @@
+/**
+ *
+ */
+package uk.co.randomcoding.drinkfinder.android
+
+import android.app.Activity
+import android.os.Bundle
+
+/**
+ * Activity to manage entering search parameters for drink finding
+ *
+ * @author RandomCoder <randomcoder@randomcoding.co.uk>
+ *
+ * Created On: 11 Jul 2012
+ *
+ */
+class SearchDrinkActivity extends Activity {
+
+	override def onCreate(savedInstanceState: Bundle) {
+		super.onCreate(savedInstanceState)
+	}
+}
