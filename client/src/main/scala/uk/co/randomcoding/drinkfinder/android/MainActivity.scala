9b35295bf40fb4913b139c8486950e7d909e1ad1
randomcoder <randomcoder@randomcoding.co.uk>
2012-07-27 07:48:55 +0100

    re #1: Added intent to transition to search activity

--- client/src/main/scala/uk/co/randomcoding/drinkfinder/android/MainActivity.scala
+++ client/src/main/scala/uk/co/randomcoding/drinkfinder/android/MainActivity.scala
@@ -20,26 +20,27 @@
 package uk.co.randomcoding.drinkfinder.android
 
 import android.app.Activity
+import android.content.Intent
 import android.os.Bundle
-import uk.co.randomcoding.drinkfinder.android._
 import android.view.View
 
 class MainActivity extends Activity with TypedActivity {
   override def onCreate(bundle: Bundle) {
     super.onCreate(bundle)
     setContentView(R.layout.main)
-  
+
   }
-  
+
   def showSearchDrinks(view: View) {
-	  // TODO: create intent to switch to the search view
+    val intent = new Intent(this, classOf[SearchDrinkActivity])
+    startActivity(intent)
   }
-  
+
   def updateData(view: View) {
-	  // TODO: Update the data from the online store and save in zipped file
+    // TODO: Update the data from the online store and save in zipped file
   }
-  
+
   def showWishList(view: View) {
-	  // TODO: Create intent to switch to the wishlist view
+    // TODO: Create intent to switch to the wishlist view
   }
 }
