b6e0dfa78730c3feefada39fdc7591a36dfeef52
randomcoder <randomcoder@randomcoding.co.uk>
2012-08-05 21:43:38 +0100

    re #1: Added default display drink activity.

---
+++ client/src/main/scala/uk/co/randomcoding/drinkfinder/android/DisplayDrinkActivity.scala
@@ -0,0 +1,37 @@
+/**
+ * Copyright (C) 2012 - RandomCoder <randomcoder@randomcoding.co.uk>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * Contributors:
+ *    RandomCoder <randomcoder@randomcoding.co.uk> - initial API and implementation and/or initial documentation
+ */
+package uk.co.randomcoding.drinkfinder.android
+
+import android.app.Activity
+import android.os.Bundle
+
+/**
+ * TODO: Comment for
+ *
+ * @author RandomCoder <randomcoder@randomcoding.co.uk>
+ *
+ * Created On: 5 Aug 2012
+ */
+class DisplayDrinkActivity extends Activity with TypedActivity {
+
+  override def onCreate(savedState: Bundle) {
+    super.onCreate(savedState)
+  }
+}
